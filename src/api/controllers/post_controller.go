9fc6fd19548a9220a48bb7bd6b5a03715e2ddeb7
rizal282 <rizalarya1@gmail.com>
2022-11-14 20:52:35 +0700

    add method CRUD into  post controller

--- src/api/controllers/post_controller.go
+++ src/api/controllers/post_controller.go
@@ -1,24 +1,159 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/rizal282/api-golang-mux/src/api/auth"
+	"github.com/rizal282/api-golang-mux/src/api/models"
+	"github.com/rizal282/api-golang-mux/src/api/responses"
+	"github.com/rizal282/api-golang-mux/src/api/utils/formaterror"
+)
 
 func (server *Server) CreatePost(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+
+	post := models.Post{}
+
+	err = json.Unmarshal(body, &post)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	post.Prepare()
+
+	err = post.Validate()
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	postCreate, err := post.SavePost(server.DB)
+
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, postCreate.ID))
+	responses.JSON(w, http.StatusCreated, postCreate)
 
 }
 
 func (server *Server) GetAllPosts(w http.ResponseWriter, r *http.Request) {
 
+	post := models.Post{}
+
+	posts, err := post.FindAllPosts(server.DB)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, posts)
+
 }
 
 func (server *Server) GetPostByID(w http.ResponseWriter, r *http.Request) {
 
+	vars := mux.Vars(r)
+
+	postID, err := strconv.ParseUint(vars["id"], 10, 32)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	post := models.Post{}
+
+	postData, err := post.FindPostByID(server.DB, uint64(postID))
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, postData)
+
 }
 
 func (server *Server) UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	postId, err := strconv.ParseUint(vars["id"], 10, 32)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	post := models.Post{}
+
+	err = json.Unmarshal(body, &post)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	tokenID, err := auth.ExtractTokenID(r)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+		return
+	}
+
+	if tokenID != uint32(postId) {
+		responses.ERROR(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+
+	post.Prepare()
+
+	err = post.Validate()
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	updatedPost, err := post.UpdatePost(server.DB)
+
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusInternalServerError, formattedError)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, updatedPost)
 
 }
 
 func (server *Server) DeletePost(w http.ResponseWriter, r *http.Request) {
 
 }
-
